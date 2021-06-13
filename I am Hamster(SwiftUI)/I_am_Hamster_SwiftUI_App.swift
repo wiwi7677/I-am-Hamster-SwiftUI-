5a3577d3fb6dc9ae8c3240ebad60f00a120341eb
Wiwi <wiwi880331@gmail.com>
2021-06-13 18:13:08 +0800

    Initial Commit

---
+++ I am Hamster(SwiftUI)/I_am_Hamster_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Hamster_SwiftUI_App.swift
+//  I am Hamster(SwiftUI)
+//
+//  Created by wiwi on 2021/6/13.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Hamster_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
